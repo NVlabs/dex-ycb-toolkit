4c72bab6d877c2e4086df5882bdcd1f614db7589
Yu-Wei Chao <ychao@nvidia.com>
2020-10-21 14:52:12 -0700

    Log COCO evaluation result following detectron2

--- lib/coco_eval.py
+++ lib/coco_eval.py
@@ -4,9 +4,11 @@ import numpy as np
 import pycocotools.mask
 import json
 import copy
+import itertools
 
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
+from tabulate import tabulate
 
 from lib.factory import get_dataset
 
@@ -21,12 +23,14 @@ class COCOEvaluator():
 
     self._dataset = get_dataset(self._name)
 
+    self._class_names = self._dataset.ycb_classes + ['hand']
+
     self._anno_file = os.path.join(os.path.dirname(__file__), "..", "eval",
                                    "anno_coco_{}.json".format(self._name))
     if os.path.isfile(self._anno_file):
-      print('Found COCO annnotation file')
+      print('Found COCO annnotation file.')
     else:
-      print('Cannot find COCO annnotation file')
+      print('Cannot find COCO annnotation file.')
       self._generate_anno_file()
 
   def _generate_anno_file(self):
@@ -91,31 +95,24 @@ class COCOEvaluator():
 
     categories = []
 
-    for i, x in enumerate(self._dataset.ycb_classes):
-      if i == 0:
-        continue
+    for i, x in enumerate(self._class_names):
+      if x == 'hand':
+        supercategory = 'mano'
+        keypoints = self._dataset.mano_joints
+        skeleton = [[y + 1 for y in x] for x in self._dataset.mano_joint_connect
+                   ]
+      else:
+        supercategory = 'ycb'
+        keypoints = []
+        skeleton = []
       cat = {
-          'id': i,
+          'id': i + 1,
           'name': x,
-          'supercategory': 'ycb',
-          'keypoints': [],
-          'skeleton': [],
+          'supercategory': supercategory,
+          'keypoints': keypoints,
+          'skeleton': skeleton,
       }
       categories.append(cat)
-
-    cat = {
-        'id':
-            22,
-        'name':
-            'hand',
-        'supercategory':
-            'mano',
-        'keypoints':
-            self._dataset.mano_joints,
-        'skeleton':
-            [[y + 1 for y in x] for x in self._dataset.mano_joint_connect],
-    }
-    categories.append(cat)
 
     anno = {}
     anno['info'] = {}
@@ -133,13 +130,67 @@ class COCOEvaluator():
     e = time.time()
     print('time: {:7.2f}'.format(e - s))
 
-  def evaluate(self, res_file):
+  # https://github.com/facebookresearch/detectron2/blob/492cf9c7bae22d7d528f7f58169fcd52a450a0ca/detectron2/evaluation/coco_evaluation.py#L252
+  def _derive_coco_results(self, coco_eval, iou_type):
+    metrics = {
+        'bbox': ['AP', 'AP50', 'AP75', 'APs', 'APm', 'APl'],
+        'segm': ['AP', 'AP50', 'AP75', 'APs', 'APm', 'APl'],
+        'keypoints': ['AP', 'AP50', 'AP75', 'APm', 'APl'],
+    }[iou_type]
+
+    results = {
+        metric: float(coco_eval.stats[idx] *
+                      100 if coco_eval.stats[idx] >= 0 else "nan")
+        for idx, metric in enumerate(metrics)
+    }
+    keys, values = tuple(zip(*results.items()))
+    table = tabulate(
+        [values],
+        headers=keys,
+        tablefmt='pipe',
+        floatfmt='.3f',
+        stralign='center',
+        numalign='center',
+    )
+    print('Evaluation results for {}: \n'.format(iou_type) + table)
+    if not np.isfinite(sum(results.values())):
+      print('Some metrics cannot be computed and is shown as NaN.')
+
+    precisions = coco_eval.eval["precision"]
+    assert len(self._class_names) == precisions.shape[2]
+
+    results_per_category = []
+    for idx, name in enumerate(self._class_names):
+      precision = precisions[:, :, idx, 0, -1]
+      precision = precision[precision > -1]
+      ap = np.mean(precision) if precision.size else float('nan')
+      results_per_category.append(("{}".format(name), float(ap * 100)))
+
+    n_cols = min(6, len(results_per_category) * 2)
+    results_flatten = list(itertools.chain(*results_per_category))
+    results_2d = itertools.zip_longest(
+        *[results_flatten[i::n_cols] for i in range(n_cols)])
+    table = tabulate(
+        results_2d,
+        tablefmt='pipe',
+        floatfmt='.3f',
+        headers=['category', 'AP'] * (n_cols // 2),
+        numalign='left',
+    )
+    print('Per-category {} AP: \n'.format(iou_type) + table)
+
+    results.update({'AP-' + name: ap for name, ap in results_per_category})
+    return results
+
+  def evaluate(self, res_file, tasks=('bbox', 'segm', 'keypoints')):
     coco_gt = COCO(self._anno_file)
     coco_dt = coco_gt.loadRes(res_file)
 
-    for ann_type in ('bbox', 'segm', 'keypoints'):
-      # https://github.com/facebookresearch/detectron2/blob/d2b028ac3a80ccf6e7bb37acc0229b23c04125c0/detectron2/evaluation/coco_evaluation.py#L506
-      if ann_type == 'segm':
+    results = {}
+
+    for task in tasks:
+      # https://github.com/facebookresearch/detectron2/blob/492cf9c7bae22d7d528f7f58169fcd52a450a0ca/detectron2/evaluation/coco_evaluation.py#L506
+      if task == 'segm':
         coco_dt_ = copy.deepcopy(coco_dt)
         for ann in coco_dt_.loadAnns(coco_dt_.getAnnIds()):
           ann.pop('bbox', None)
@@ -147,13 +198,15 @@ class COCOEvaluator():
       else:
         coco_dt_ = coco_dt
 
-      coco_eval = COCOeval(coco_gt, coco_dt_, ann_type)
+      coco_eval = COCOeval(coco_gt, coco_dt_, task)
 
-      if ann_type == 'keypoints':
+      if task == 'keypoints':
         coco_eval.params.kpt_oks_sigmas = np.array(_KPT_OKS_SIGMAS)
 
       coco_eval.evaluate()
       coco_eval.accumulate()
       coco_eval.summarize()
 
-    return coco_eval
+      results[task] = self._derive_coco_results(coco_eval, task)
+
+    return results
