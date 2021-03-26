da80e9bb6271d07b8d0ae4c540f45a0a32d66616
Yu-Wei Chao <ychao@nvidia.com>
2021-03-26 08:41:17 -0700

    Add checksum

--- results/fetch_example_results.sh
+++ results/fetch_example_results.sh
@@ -5,10 +5,22 @@ cd $DIR
 
 FILE=example_results.tar.gz
 ID=1b_rS0EHoji9a41c17v2hMQKMnMoM1oC4
+CHECKSUM=93853ec3a1cddce2f459ababd5c42a6a
 
 if [ -f $FILE ]; then
-  echo "File already exists."
-  exit 0
+  echo "File already exists. Checking md5..."
+  os=`uname -s`
+  if [ "$os" = "Linux" ]; then
+    checksum=`md5sum $FILE | awk '{ print $1 }'`
+  elif [ "$os" = "Darwin" ]; then
+    checksum=`cat $FILE | md5`
+  fi
+  if [ "$checksum" = "$CHECKSUM" ]; then
+    echo "Checksum is correct. No need to download."
+    exit 0
+  else
+    echo "Checksum is incorrect. Need to download again."
+  fi
 fi
 
 echo "Downloading example results (3M)..."
