ad3871e23f20c110b3778c20c8afb257f9c92a38
Neil Song <song.neil.song@gmail.com>
2022-05-13 16:35:47 -0500

    Update setup.py for later python version (#17)
    * Update setup.py for later python version
    
    Current install hangs on at least 3.7.13, unsure about older versions of 3.7
    Update allows for successful install up to latest (3.10.x)
    
    * remove include_package_data

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 # Copyright (C) 2021 NVIDIA Corporation
 # Licensed under the GNU General Public License v3.0 [see LICENSE for details]
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='dex-ycb-toolkit',
     version='1.0',
+    packages=find_packages(),
     install_requires=[
         'chumpy',
         'numpy',
